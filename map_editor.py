0f1ce990ece60b99cf81b94403cc3bd607db8657
Rpergy <rpergola0729@gmail.com>
2023-03-06 19:28:28 -0500

    changes to map editor & fixed pathfinding bugs

--- map_editor.py
+++ map_editor.py
@@ -16,7 +16,11 @@ verticies = []
 edges = []
 
 def main():
+    showImg = True
+
     floatingLine = None
+
+    font = pygame.font.Font("freesansbold.ttf", 20)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,6 +28,8 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     floatingLine = None
+                if event.key == pygame.K_s:
+                    showImg = not showImg
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     verticies.append(mousePos)
@@ -45,16 +51,22 @@ def main():
                                 break
 
         gameDisplay.fill((255,255,255))
-        gameDisplay.blit(img, (0,0))
+        if showImg: gameDisplay.blit(img, (0,0))
 
         mousePos = pygame.mouse.get_pos()
         pygame.draw.circle(gameDisplay, (255, 0, 0), mousePos, 10)
-
-        for i in verticies:
-            pygame.draw.circle(gameDisplay, (255, 0, 0), i, 10)
         
         for i in edges:
             pygame.draw.line(gameDisplay, (0, 0, 0), verticies[i[0]], verticies[i[1]], width=3)
+
+        for i in range(len(verticies)):
+            pygame.draw.circle(gameDisplay, (255, 0, 0), verticies[i], 10)
+
+            text = font.render(str(i), True, (255, 255, 255))
+            textRect = text.get_rect()
+            textRect.center = verticies[i]
+
+            gameDisplay.blit(text, textRect)
 
         if not floatingLine == None:
             pygame.draw.line(gameDisplay, (0, 0, 0), verticies[floatingLine], mousePos, width=3)
@@ -76,10 +88,25 @@ print(adjacency_table)
 quit()
 """
 
-[[  0,   0,   0, 134, 0], 
- [ 83,   0,   0,   0, 0], 
- [176,   0,   0,   0, 0], 
- [134,   0, 240,   0, 0], 
- [  0, 109,   0,  69, 0]]
+[[  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0, 303,   0,  0,  0,   0,  0, 0,  0], 
+ [ 77,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0, 94,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,  69,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0, 91,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0, 275,  0, 0,  0, 212,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0, 52, 0,  0,   0,   0,   0, 53, 137,   0,  0,  0,   0,  0, 0, 97], 
+ [  0,  82,  0, 64,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0, 57,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [164,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0, 190,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0, 173,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0, 116,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [123,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0, 153,  0,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0, 174,   0,  0,   0,   0, 60,  0,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0, 68,   0,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0, 104,  0, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0,  0,   0, 74, 0,  0], 
+ [  0,   0,  0,  0,   0,  0, 0,  0,   0,   0,   0,  0,   0,   0,  0, 92,   0,  0, 0,  0]]
 
 """
